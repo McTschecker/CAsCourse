402e9a9f73a61c1f496ee2ef971b8e6894590bb4
McTschecker <10790084+McTschecker@users.noreply.github.com>
2018-09-22 15:43:03 -0400

    Functions

---
+++ 5 Functions/functions.py
@@ -0,0 +1,15 @@
+### Copyright Fabian Blank
+
+## Now you will learn how to use functions in python
+## example for hello world
+def helloWorld():
+    print('Hello World!')
+
+# Now create a function, named "doubleNumber", which doubles a number and then returns it
+# Please keep in mind, that the variable given to the Function
+
+
+
+
+
+# Now create a function, named "rootDoube", which finds the square root of a number and then doubles it. Please then return the final number
