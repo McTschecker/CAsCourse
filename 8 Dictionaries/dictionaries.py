2b7329f5150642e3f3c5334f4cd58e1500605e4d
McTschecker <10790084+McTschecker@users.noreply.github.com>
2018-10-22 08:33:12 -0400

    Added Dictiionaries Episode

---
+++ 8 Dictionaries/dictionaries.py
@@ -0,0 +1,18 @@
+### Copyright Fabian Blank
+# In thic chapter you will work with Dictionaries
+# Dictionaries, help you with tasks of translation of input
+# To make a dictionary, please use the following syntax
+
+dictionary = {
+    "a" : 1,
+    "b" : 2,
+}
+
+#Your Task
+# Please write a Dictionary in a function called translate
+# Your dictionary should translate the following names to their respective phone numbers
+
+
+# "Jack" to "012314243423"
+# "Tom" to 012314234323432
+# "AnotherPerson" to 01231474747
